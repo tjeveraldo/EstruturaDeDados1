274945d5dda1c8a9dc9b2ffa0996dab587ae064b
tjeveraldo <36521432+tjeveraldo@users.noreply.github.com>
2018-02-22 19:58:30 -0300

    Add files via upload

---
+++ casa.c
@@ -0,0 +1,28 @@
+typedef struct cmd{
+	char nome[15];
+	float l1;
+	float l2;
+	float area;
+}comodo;
+
+typedef struct casa{
+	 comodo vetor[5];
+}casa;
+
+int main(){
+	
+	int i;
+	casa m_casa;
+	
+	
+	
+	for(i = 0; i <= 4; i++){
+		scanf("%s",m_casa[i].nome);
+	}
+	
+	
+}
+
+
+
+
